4af92f22c741c3a4c9c5cca84d47846c9d4c2cd8
yaojiach <jiachen.yao@outlook.com>
2020-10-18 23:39:32 -0400

    Modernize for 2020

--- README.md
+++ README.md
@@ -2,18 +2,22 @@
 
 Dockerize a Python Dash app for quick prototyping.
 
+## Build and run
+
+```sh
+docker build -t dash .
+
+docker run -p 8050:8050 \
+-v "$(pwd)"/app:/app \
+--rm dash
+```
+
+## Access the page
+
+Go to `http://localhost:8050` in browser.
+
 ## Switch debug mode in Dockerfile
 
 ```dockerfile
 ENV DASH_DEBUG_MODE True # False
 ```
-
-## Build and run
-
-```sh
-docker build -t dash . && docker run --rm -p 8050:8050 dash
-```
-
-## Access the page
-
-Go to `http://0.0.0.0:8050/` in browser.
--- app/app.py
+++ app/app.py
@@ -6,46 +6,38 @@ import flask
 
 
 server = flask.Flask(__name__)
-app = dash.Dash(
-    __name__,
-    server=server,
-    external_stylesheets=[
-        dbc.themes.BOOTSTRAP
-    ]
-)
+app = dash.Dash(__name__, server=server, external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.config.suppress_callback_exceptions = True
 
-
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-
-    html.Div(children='''
+app.layout = html.Div(
+    children=[
+        html.H1(children="Hello Dash"),
+        html.Div(
+            children="""
         Dash: A web application framework for Python.
-    '''),
+    """
+        ),
+        dcc.Graph(
+            id="example-graph",
+            figure={
+                "data": [
+                    {"x": [1, 2, 3], "y": [4, 1, 2], "type": "bar", "name": "SF"},
+                    {
+                        "x": [1, 2, 3],
+                        "y": [2, 4, 5],
+                        "type": "bar",
+                        "name": u"Montréal",
+                    },
+                ],
+                "layout": {"title": "Dash Data Visualization"},
+            },
+        ),
+    ]
+)
 
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'title': 'Dash Data Visualization'
-            }
-        }
-    )
-])
 
-
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     import os
 
-    debug = False if os.environ['DASH_DEBUG_MODE'] == 'False' else True
-
-    app.run_server(
-        host='0.0.0.0',
-        port=8050,
-        debug=debug
-    )
+    debug = False if os.environ["DASH_DEBUG_MODE"] == "False" else True
+    app.run_server(host="0.0.0.0", port=8050, debug=debug)
